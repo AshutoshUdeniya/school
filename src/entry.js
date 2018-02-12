c078aa61a932836db2ffe51f5831c0227614410a
AshutoshUdeniya <ashutosh.udeniya@carwale.com>
2018-02-12 23:00:45 +0530

    WIP: webpack settings

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+render(<p>it's my entry point</p>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
-module.export = {}
+module.export = {
+
+    entry: __dirname + 'index.html',
+
+    module: { //Each "loader" that you add to the loaders array will represent 
+              //a transformation that your code will go through before reaching the browser
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    output: {
+        filename: 'transformed.js',
+        path: __dirname + '/build'
+    }
+};
